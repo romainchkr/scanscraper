0b3ac6b2441bd78f578c7438ca0514f6f2648429
Romain Chikirou <romanochiki5@gmail.com>
2021-11-28 23:24:13 +0100

    #2: new functionnality -> create pdf

--- scanscraper/pipelines.py
+++ scanscraper/pipelines.py
@@ -7,4 +7,4 @@ from scrapy.pipelines.images import ImagesPipeline
 
 class customImagePipeline(ImagesPipeline):
     def file_path(self, request, response=None, info=None, *, item=None):
-        return request.url.split('/')[-4] + "_" + request.url.split('/')[-2] + "_" + request.url.split('/')[-1]
+        return item['manga_name'] + '/' + item['chapter'] + '/' + request.url.split('/')[-1]
--- scanscraper/spiders/scanspider.py
+++ scanscraper/spiders/scanspider.py
@@ -1,4 +1,6 @@
 import scrapy
+from  scanscraper.items import ChapterItem
+
 import logging
 import os
 import re
@@ -7,21 +9,24 @@ import json
 # execute script : scrapy crawl scan 
 class ScanSpider(scrapy.Spider):
     name = "scan"
-    start_urls = ["https://www.scan-vf.net/dr-stone"]
+    current_manga_name = ""
 
     def parse(self, response):
-        logging.info("Starting scrape")
+        logging.info("Start scraping")
 
         manga_name = response.xpath('/html/body/div[1]/div/div[1]/div/h2/text()').get()
-        if not os.path.exists("Scans\\"+manga_name):
-            os.makedirs("Scans\\"+manga_name)
+        if not os.path.exists("scans\\"+manga_name):
+            os.makedirs("scans\\"+manga_name)
+        
+        self.current_manga_name = manga_name
 
-        chapters = response.xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/ul/li/h5/a/@href')
+        #chapters = response.xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/ul/li/h5/a/@href')
+        chapters = response.xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/ul/li[1]/h5/a/@href')
 
         for chapter in chapters:
             logging.debug(chapter.get())
             yield scrapy.Request(chapter.get(), callback=self.parse_chapter)
-
+    
     def parse_chapter(self, response):
         logging.debug("parse_chapter")
 
@@ -31,6 +36,4 @@ class ScanSpider(scrapy.Spider):
         base_url = base[:len(base)-base[::-1].index('/')].strip()
         image_urls = [base_url + image['page_image'] for image in image_obj]
 
-        yield {
-            'image_urls' : image_urls
-        }
+        yield ChapterItem(image_urls=image_urls, manga_name=self.current_manga_name, chapter=image_urls[0].split('/')[-2].split('-')[1])
