f2cca30e977541546728564ce91c215ebb7c44d0
Romain Chikirou <romanochiki5@gmail.com>
2021-11-29 02:35:09 +0100

    #2: Allow crawler to return data with MyStaticCrawler class

--- scanscraper.py
+++ scanscraper.py
@@ -1,3 +1,4 @@
+import logging
 from scrapy.crawler import CrawlerProcess
 from scanscraper.spiders import scanspider
 from scrapy.utils.project import get_project_settings
@@ -6,33 +7,47 @@ from urllib.parse import urljoin
 import os
 from PIL import Image
 
-def create_pdf():
+class MyStaticCrawler:
+
+    def __init__(self):
+        self.output = None
+        self.process = CrawlerProcess(get_project_settings())
+
+    def yield_output(self, data):
+        self.output = data
+
+    def crawl(self, cls, urls):
+        self.process.crawl(cls, start_urls=urls, args={'callback': self.yield_output})
+        self.process.start()
+
+def create_pdf(url):
     imageList = []
-    base_url = urljoin(os.getcwd(), 'scans/Solo Leveling/')
-    folders = os.listdir(base_url)
+    # get png and jpg downloaded files
+    files = os.listdir(url)
+    files.sort()
+    files = [file for file in files if '.jpg' in file or '.png' in file]
 
-    for folder in folders:
-        files_url = urljoin(os.getcwd(), 'scans/Solo Leveling/'+folder+'/')
-        files = os.listdir(files_url)
-        files.sort()
-        files = [file for file in files if '.jpg' in file or '.png' in file]
+    if len(files) > 0:
+        image1 = Image.open(url + files[0])
+        for filename in files[1:]:
+            imageList.append(Image.open(url + filename).convert('RGB'))
+        
+        image1.save(url + url.split('/')[-2] + '.pdf',save_all=True, append_images=imageList)
 
-        if len(files) > 0:
-            image1 = Image.open(files_url + files[0])
-            for filename in files[1:]:
-                imageList.append(Image.open(files_url + filename).convert('RGB'))
-            
-            image1.save(files_url + folder + '.pdf',save_all=True, append_images=imageList)
-            delete_img(files)
+        delete_img(files)
 
 def delete_img(files):
     pass
 
 if __name__ == '__main__':
-    start_urls = ["https://www.scan-vf.net/solo-leveling"]
+    logging.basicConfig(level=logging.DEBUG)
+    start_urls = ["https://www.scan-vf.net/solo-leveling", 'https://www.scan-vf.net/black-clover']
 
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(scanspider.ScanSpider, start_urls=start_urls)
-    process.start()
+    crawler = MyStaticCrawler()
+    crawler.crawl(scanspider.ScanSpider, start_urls)
+    data = crawler.output
+    logging.debug(data)
 
-    create_pdf()
+    for manga_name, chapters in data.items():
+        for chapter in chapters:
+            create_pdf(urljoin(os.getcwd(), 'scans/'+manga_name+'/'+chapter+'/'))
--- scanscraper/spiders/scanspider.py
+++ scanscraper/spiders/scanspider.py
@@ -6,10 +6,20 @@ import os
 import re
 import json
 
-# execute script : scrapy crawl scan 
+# to execute the crawler uncomment line : start_urls ....
+# then comment functions __init__ and close
+# then launch the following command line : scrapy crawl scan 
 class ScanSpider(scrapy.Spider):
     name = "scan"
-    current_manga_name = ""
+    output = {}
+    start_urls = ["https://www.scan-vf.net/solo-leveling"]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.output_callback = kwargs.get('args').get('callback')
+    
+    def close(self, spider, reason):
+        self.output_callback(self.output)
 
     def parse(self, response):
         logging.info("Start scraping")
@@ -18,14 +28,14 @@ class ScanSpider(scrapy.Spider):
         if not os.path.exists("scans\\"+manga_name):
             os.makedirs("scans\\"+manga_name)
         
-        self.current_manga_name = manga_name
+        self.output[manga_name] = []
 
         #chapters = response.xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/ul/li/h5/a/@href')
         chapters = response.xpath('/html/body/div[1]/div/div[1]/div/div[4]/div/ul/li[1]/h5/a/@href')
 
         for chapter in chapters:
             logging.debug(chapter.get())
-            yield scrapy.Request(chapter.get(), callback=self.parse_chapter)
+            yield scrapy.Request(chapter.get(), callback=self.parse_chapter, meta={'manga_name': manga_name})
     
     def parse_chapter(self, response):
         logging.debug("parse_chapter")
@@ -36,4 +46,7 @@ class ScanSpider(scrapy.Spider):
         base_url = base[:len(base)-base[::-1].index('/')].strip()
         image_urls = [base_url + image['page_image'] for image in image_obj]
 
-        yield ChapterItem(image_urls=image_urls, manga_name=self.current_manga_name, chapter=image_urls[0].split('/')[-2].split('-')[1])
+        chapter = image_urls[0].split('/')[-2].split('-')[1]
+        yield ChapterItem(image_urls=image_urls, manga_name=response.meta.get('manga_name'), chapter=chapter)
+
+        self.output[response.meta.get('manga_name')].append(chapter)
